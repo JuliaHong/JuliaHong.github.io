10319bdb338d9feecd6d1249a69e059ad03fcc65
JuliaHong <41278051+JuliaHong@users.noreply.github.com>
2018-08-15 20:47:31 +0900

    Added some login functions and stuff
    It's is 9pm and got shit tons of work :cocktail:
    Go Go Go (wanna drink beer)

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-
-
 var app = new Vue({
   el : '#root',
   data : {
@@ -17,3 +15,25 @@ var app = new Vue({
     }
   }
 })
+
+// 로그인 코드펜임
+
+$('.tabs .tab').click(function(){
+    if ($(this).hasClass('signin')) {
+        $('.tabs .tab').removeClass('active');
+        $(this).addClass('active');
+        $('.cont').hide();
+        $('.signin-cont').show();
+    }
+    if ($(this).hasClass('signup')) {
+        $('.tabs .tab').removeClass('active');
+        $(this).addClass('active');
+        $('.cont').hide();
+        $('.signup-cont').show();
+    }
+});
+$('.container .bg').mousemove(function(e){
+    var amountMovedX = (e.pageX * -1 / 30);
+    var amountMovedY = (e.pageY * -1 / 9);
+    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+});
